f26fe15b7634d8699429e58df4df36b96956580e
SandBot <super.khang2@gmail.com>
2021-08-13 11:59:20 +0700

    Add project files.

---
+++ Bai3/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QLKhuDat ql = new QLKhuDat();
+
+            int chon = 0;
+            do
+            {
+                Console.WriteLine("------------MENU------------");
+                Console.WriteLine("1.Tao DS.");
+                Console.WriteLine("2.Xuat DS");
+                Console.WriteLine("3.Xuat Xuat Tang Dan");
+                Console.WriteLine("4.Xuat Theo Gia Ban Va Dien Tich");
+                Console.WriteLine("5.Tinh Don Gia Trung Binh Cua Dat 1m2");
+                Console.WriteLine("0.Exit.");
+                Console.Write("Chon: ");
+                Console.WriteLine("----------------------------");
+                chon = int.Parse(Console.ReadLine());
+                switch (chon)
+                {
+                    case 1:
+                        Console.Write("Nhap so Khu Dat trong DS: ");
+                        int sl = int.Parse(Console.ReadLine());
+                        ql.NhapDSDat(sl);
+                        break;
+                    case 2:
+                        ql.XuatDSKhuDat();
+                        break;
+                    case 3:
+                        ql.XuatTangDan();
+                        break;
+                    case 4:
+                        Console.Write("Nhap Gia Ban: ");
+                        int giaban = int.Parse(Console.ReadLine());
+                        Console.Write("Nhap Dien Tich: ");
+                        float dientich = float.Parse(Console.ReadLine());
+                        ql.XuatTheoGiaBanVaDienTich(giaban,dientich);
+                        break;
+                    case 5:
+                        ql.TinhDonGiaTrungBinhCuaDat1m2();
+                        break;
+                    default:
+                        chon = 0;
+                        break;
+                }
+            } while (chon != 0);
+        }
+    }
+}
---
+++ Bai3/QLKhuDat.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai3
+{
+    class QLKhuDat
+    {
+        public List<KhuDat> KD = new List<KhuDat>();
+
+        public void NhapDSDat(int n)
+        {
+            for (int i = 0; i< n; i++)
+            {
+                KhuDat kd = new KhuDat();
+
+                kd.Input();
+
+                KD.Add(kd);
+            }
+        }
+
+        public void XuatDSKhuDat()
+        {
+            int i = 0;
+            foreach(KhuDat kd in KD)
+            {
+                Console.WriteLine(i+"."+kd.Tosting());
+                i++;
+            }
+        }
+
+
+        public void XuatDS(List<KhuDat> list)
+        {
+            int i = 0;
+            foreach (KhuDat kd in list)
+            {
+                Console.WriteLine(i + "." + kd.Tosting());
+                i++;
+            }
+        }
+
+        public void XuatTangDan()
+        {
+            List<KhuDat> list = KD.OrderBy(p => p.dientich).ToList();
+
+            XuatDS(list);
+        }
+
+        public void XuatTheoGiaBanVaDienTich(int giaban , float dientich)
+        {
+            List<KhuDat> list = KD.Where(p => p.giaBan == giaban && p.dientich == dientich).ToList();
+
+            XuatDS(list);
+        }
+
+        public void TinhDonGiaTrungBinhCuaDat1m2()
+        {
+            List<KhuDat> list = KD.Where(p => p.dientich > 1000).ToList();
+
+            float tong = list.Sum(p => p.giaBan);
+
+            Console.WriteLine("DonGia = " + tong);
+        }
+    }
+}
